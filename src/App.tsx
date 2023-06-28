106880fb68574b80b0eac945ad720f7649d71413
orellanarodrigo <rodrigo.orellana.1982@gmail.com>
2023-06-28 11:34:59 -0400

    dark mode ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css'
 import Table from './components/table';
 import { Jokes as JokePage } from './pages/jokes';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import ProtectedRoute from './config/protectedRoute';
 import useAuth from './hooks/auth';
+import useDarkMode from './hooks/darkMode';
 
 function App() {
 
   const ListJokes = () => {
     const { user, signOut } = useAuth();
+    const { isDarkMode, toggleDarkMode } = useDarkMode();
 
     return (
       <div className="App">
@@ -16,7 +19,9 @@ function App() {
           <div><h1> Prueba Tecnica para Acid Labs 2023 </h1></div>
           <div>
             <p>{user?.name}</p>
-            <a href='/' onClick={signOut}>Cerrar sesion</a>
+            <a href='/' onClick={signOut}>Cerrar sesion</a> 
+            <a href='#' onClick={toggleDarkMode}>Cambiar a {isDarkMode? 'Light': 'Dark'} </a> 
+            {/* PULGX ETA ACTUALI_nDO L PGINA!! */}
           </div>
         </header>
         <main>
---
+++ src/hooks/darkMode.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('darkMode') === 'true');
+
+  useEffect(() => {
+    console.log('useEffect', isDarkMode);
+    setTheme(() => {
+      console.log('initiateTheme OK', isDarkMode);
+    });
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    localStorage.setItem('darkMode', !isDarkMode ? 'true' : 'false');
+    setIsDarkMode(!isDarkMode);
+    // setTheme(() => {
+    //   console.log('toggleDarkMode OK', isDarkMode);
+    // });
+  };
+
+  const setTheme = (scriptLoaded: any) => {
+    const styleLoaded = async () => {
+    };
+
+    const rootCSS = `https://cdn.jsdelivr.net/npm/water.css@2/out/`;
+    const targetCSS = !isDarkMode ? 'light.css' : 'dark.css';
+    const themeCSS = document.createElement('link');
+    themeCSS.id = 'js-stylesheet';
+    themeCSS.rel = 'stylesheet';
+    themeCSS.onload = styleLoaded;
+    themeCSS.href = rootCSS + targetCSS;
+    document.head.appendChild(themeCSS);
+    console.log('initiateTheme 1', { isDarkMode, targetCSS });
+    scriptLoaded(true);
+    return () => {
+      document.body.removeChild(themeCSS);
+    };
+  };
+
+  return { isDarkMode, toggleDarkMode };
+};
+
+export default useDarkMode;
